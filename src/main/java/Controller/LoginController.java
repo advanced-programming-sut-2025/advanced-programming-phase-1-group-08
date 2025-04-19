ab3ad9b6a2a9089dec51a8c5131ed4f6faea44ea
erfan <Erfansedaghat2321@gmail.com>
2025-04-19 21:38:29 +0330

    addtest12312

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -2,21 +2,4 @@ package Controller;
 
 public class LoginController {
 
-    public int test (int x) {
-
-        x = x*3;
-        x--;
-        return x;
-    }
-    public String check () {
-        String str = "alaki";
-        return str;
-    }
-    public void alaki () {
-
-        int x = 0;
-        x = 23;
-        x++;
-
-    }
 }
