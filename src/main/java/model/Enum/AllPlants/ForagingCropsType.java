d70e4aa79fb98ab69dca76f5e48f746bf1ac82b0
erfan <Erfansedaghat2321@gmail.com>
2025-04-27 12:43:45 +0330

    add ForagingCropsType

--- src/main/java/model/Enum/AllPlants/ForagingCropsType.java
+++ src/main/java/model/Enum/AllPlants/ForagingCropsType.java
@@ -1,6 +1,90 @@
 package model.Enum.AllPlants;
 
+import model.Enum.WeatherTime.Season;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public enum ForagingCropsType {
 
+    CommonMushroom  ("Common Mushroom", 40, 38, "Icon_CommonMushroom",
+            List.of(Season.Spring, Season.Fall, Season.Summer, Season.Winter)),
+    Daffodil        ("Daffodil",        30, 0,  "Icon_Daffodil",
+            List.of(Season.Spring)),
+    Dandelion       ("Dandelion",       40, 25, "Icon_Dandelion",
+            List.of(Season.Spring)),
+    Leek            ("Leek",            60, 40, "Icon_Leek",
+            List.of(Season.Spring)),
+    Morel           ("Morel",           150,20, "Icon_Morel",
+            List.of(Season.Spring)),
+    Salmonberry     ("Salmonberry",     5,  25, "Icon_Salmonberry",
+            List.of(Season.Spring)),
+    SpringOnion     ("Spring Onion",    8,  13, "Icon_SpringOnion",
+            List.of(Season.Spring)),
+    WildHorseradish ("Wild Horseradish",50, 13, "Icon_WildHorseradish",
+            List.of(Season.Spring)),
+    FiddleheadFern  ("Fiddlehead Fern", 90, 25, "Icon_FiddleheadFern",
+            List.of(Season.Summer)),
+    Grape           ("Grape",           80, 38, "Icon_Grape",
+            List.of(Season.Summer)),
+    RedMushroom     ("Red Mushroom",    75, -50,"Icon_RedMushroom",
+            List.of(Season.Summer)),
+    SpiceBerry      ("Spice Berry",     80, 25, "Icon_SpiceBerry",
+            List.of(Season.Summer)),
+    SweetPea        ("Sweet Pea",       50, 0,  "Icon_SweetPea",
+            List.of(Season.Summer)),
+    Blackberry      ("Blackberry",      25, 25, "Icon_Blackberry",
+            List.of(Season.Fall)),
+    Chanterelle     ("Chanterelle",     160,75, "Icon_Chanterelle",
+            List.of(Season.Fall)),
+    Hazelnut        ("Hazelnut",        40, 38, "Icon_Hazelnut",
+            List.of(Season.Fall)),
+    PurpleMushroom  ("Purple Mushroom", 90, 30, "Icon_PurpleMushroom",
+            List.of(Season.Fall)),
+    WildPlum        ("Wild Plum",       80, 25, "Icon_WildPlum",
+            List.of(Season.Fall)),
+    Crocus          ("Crocus",          60, 0,  "Icon_Crocus",
+            List.of(Season.Winter)),
+    CrystalFruit    ("Crystal Fruit",   150,63, "Icon_CrystalFruit",
+            List.of(Season.Winter)),
+    Holly           ("Holly",           80, -37,"Icon_Holly",
+            List.of(Season.Winter)),
+    SnowYam         ("Snow Yam",        100,30, "Icon_SnowYam",
+            List.of(Season.Winter)),
+    WinterRoot      ("Winter Root",     70, 25, "Icon_WinterRoot",
+            List.of(Season.Winter));
 
+    private final String displayName;
+    private final int price;
+    private final int energy;
+    private final String icon;
+    private final List<Season> seasons;
+
+    ForagingCropsType (String displayName, int price, int energy, String icon, List<Season> seasons) {
+        this.displayName = displayName;
+        this.price = price;
+        this.energy = energy;
+        this.icon = icon;
+        this.seasons = seasons;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public ArrayList<Season> getSeason() {
+        return new ArrayList<>(seasons);
+    }
 }
--- src/main/java/model/Enum/AllPlants/TreeType.java
+++ src/main/java/model/Enum/AllPlants/TreeType.java
@@ -1,46 +1,59 @@
 package model.Enum.AllPlants;
 
+import static model.Color_Eraser.*;
 import static model.Enum.AllPlants.TreesSourceType.*;
 
 public enum TreeType {
 
-    ApricotTree     ("Apricot Tree",  Apricot_Sapling,  TreesProductType.Apricot, 1),
-    CherryTree      ("Cherry Tree",   Cherry_Sapling,   TreesProductType.Cherry,  1),
-    BananaTree      ("Banana Tree",   Banana_Sapling,   TreesProductType.Banana,  1),
-    MangoTree       ("Mango Tree",    Mango_Sapling,    TreesProductType.Mango,   1),
-    OrangeTree      ("Orange Tree",   Orange_Sapling,   TreesProductType.Orange,  1),
-    PeachTree       ("Peach Tree",    Peach_Sapling,    TreesProductType.Peach,   1),
-    AppleTree       ("Apple Tree",    Apple_Sapling,    TreesProductType.Apple,   1),
-    OakTree         ("Oak Tree",      Acorns,           TreesProductType.OakResin,7),
-    MapleTree       ("Maple Tree",    Maple_Seeds,      TreesProductType.MapleSyrup,9),
-    PineTree        ("Pine Tree",     Pine_Cones,       TreesProductType.PineTar, 5),
-    MahoganyTree    ("Mahogany Tree", Mahogany_Seeds,   TreesProductType.Sap,     1),
-    MysticTree      ("Mystic Tree",   Mystic_Tree_Seeds,TreesProductType.MysticSyrup,7),
-    MushroomTree    ("Mushroom Tree", Mushroom_Tree_Seeds,    TreesProductType.CommonMushroom,1),
-    PomegranateTree ("Pomegranate Tree", Pomegranate_Sapling, TreesProductType.Pomegranate,   1);
+    ApricotTree     ("Apricot Tree",  Apricot_Sapling,  TreesProductType.Apricot,
+            1,BG_BRIGHT_WHITE+BLACK+BOLD+"A", BG_BRIGHT_WHITE+GREEN+BOLD+"A"),
+    CherryTree      ("Cherry Tree",   Cherry_Sapling,   TreesProductType.Cherry,
+            1,BG_BRIGHT_WHITE+BLACK+BOLD+"C", BG_BRIGHT_WHITE+GREEN+BOLD+"C"),
+    BananaTree      ("Banana Tree",   Banana_Sapling,   TreesProductType.Banana,
+            1,BG_BRIGHT_WHITE+BLACK+BOLD+"B", BG_BRIGHT_WHITE+GREEN+BOLD+"B"),
+    MangoTree       ("Mango Tree",    Mango_Sapling,    TreesProductType.Mango,
+            1,BG_BRIGHT_WHITE+BLACK+BOLD+"M", BG_BRIGHT_WHITE+GREEN+BOLD+"M"),
+    OrangeTree      ("Orange Tree",   Orange_Sapling,   TreesProductType.Orange,
+            1,BG_BRIGHT_WHITE+BLACK+BOLD+"O", BG_BRIGHT_WHITE+GREEN+BOLD+"O"),
+    PeachTree       ("Peach Tree",    Peach_Sapling,    TreesProductType.Peach,
+            1,BG_BRIGHT_WHITE+BLACK+BOLD+"P", BG_BRIGHT_WHITE+GREEN+BOLD+"P"),
+    AppleTree       ("Apple Tree",    Apple_Sapling,    TreesProductType.Apple,
+            1,BG_WHITE+BLACK+BOLD+"A", BG_WHITE+GREEN+BOLD+"A"),
+    OakTree         ("Oak Tree",      Acorns,           TreesProductType.OakResin,
+            7,BG_WHITE+BLACK+BOLD+"O", BG_WHITE+GREEN+BOLD+"O"),
+    MapleTree       ("Maple Tree",    Maple_Seeds,      TreesProductType.MapleSyrup,
+            9,BG_WHITE+BLACK+BOLD+"M", BG_WHITE+GREEN+BOLD+"M"),
+    PineTree        ("Pine Tree",     Pine_Cones,       TreesProductType.PineTar,
+            5,BG_WHITE+BLACK+BOLD+"P", BG_WHITE+GREEN+BOLD+"P"),
+    MahoganyTree    ("Mahogany Tree", Mahogany_Seeds,   TreesProductType.Sap,
+            1,BG_WHITE+BRIGHT_WHITE+BOLD+"M", BG_WHITE+BROWN+BOLD+"M"),
+    MysticTree      ("Mystic Tree",   Mystic_Tree_Seeds,TreesProductType.MysticSyrup,
+            7,BG_BRIGHT_WHITE+PURPLE+BOLD+"M", BG_BRIGHT_WHITE+BROWN+BOLD+"M"),
+    MushroomTree    ("Mushroom Tree", Mushroom_Tree_Seeds,    TreesProductType.CommonMushroom,
+            1,BG_BLUE+BLACK+BOLD+"M", BG_BLUE+GREEN+BOLD+"M"),
+    PomegranateTree ("Pomegranate Tree", Pomegranate_Sapling, TreesProductType.Pomegranate,
+            1,BG_WHITE+BROWN+BOLD+"P", BG_WHITE+RED+BOLD+"P");
 
     private final String displayName;
     private final TreesSourceType sourceType;
     private final TreesProductType productType;
     private final int harvestYield;
+    private final String icon1;
+    private final String icon2;
 
-    TreeType (String displayName, TreesSourceType sourceType, TreesProductType productType, int harvestYield) {
+    TreeType (String displayName, TreesSourceType sourceType, TreesProductType productType,
+              int harvestYield, String icon1, String icon2) {
+
         this.displayName = displayName;
         this.sourceType = sourceType;
         this.productType = productType;
         this.harvestYield = harvestYield;
+        this.icon1 = icon1;
+        this.icon2 = icon2;
     }
 
-    public String getDisplayName() {
-        return displayName;
-    }
-    public TreesSourceType getSourceType() {
-        return sourceType;
-    }
-    public TreesProductType getProductType() {
-        return productType;
-    }
-    public int getHarvestYield() {
-        return harvestYield;
-    }
+    public String getDisplayName() {return displayName;}
+    public TreesSourceType getSourceType() {return sourceType;}
+    public TreesProductType getProductType() {return productType;}
+    public int getHarvestYield() {return harvestYield;}
 }
