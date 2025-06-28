4f5ff378e563a10fc307cd51de5ae76470415ee4
erfan <Erfansedaghat2321@gmail.com>
2025-06-28 12:32:08 +0330

    fix crow

--- src/main/java/Controller/MainGame/CraftingController.java
+++ src/main/java/Controller/MainGame/CraftingController.java
@@ -126,7 +126,7 @@ public class CraftingController {
     }
     public Result goToGameMenu() {
 
-        String test = "ddsdsdsfgsdfgdsdfd";
+        String test = "ddgsdfgdsdfd";
         App.currentMenu= Menu.GameMenu;
         return new Result(true , BLUE + "Back to Game Menu" + RESET);
     }
