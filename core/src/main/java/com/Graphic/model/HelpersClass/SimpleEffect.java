e4391c79a0db6c9bdf4c3317cfb182ec478b1a91
erfan <Erfansedaghat2321@gmail.com>
2025-06-30 10:29:40 +0330

    change jdk

--- core/src/main/java/com/Graphic/model/HelpersClass/SimpleEffect.java
+++ core/src/main/java/com/Graphic/model/HelpersClass/SimpleEffect.java
@@ -5,35 +5,54 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
 
 public class SimpleEffect {
 
     float x, y;
     float stateTime = 0f;
-    static Animation<TextureRegion> animation = null;
+    static ObjectMap<String, Animation<TextureRegion>> animations = new ObjectMap<>();
+    static ObjectMap<String, Integer> frameCounts = new ObjectMap<>();
+    static float defaultSpeed = 0.5f;
 
-    public SimpleEffect (float x, float y) {
+    public SimpleEffect(float x, float y) {
         this.x = x;
         this.y = y;
+    }
 
-        if (animation == null) {
+    public static void loadAnimation(String effectName, String basePath, int frameCount, float speed) {
+        if (!animations.containsKey(effectName)) {
             Array<TextureRegion> frames = new Array<>();
-            for (int i = 0; i < 6; i++) {
-                Texture texture = new Texture("death/death" + i + ".png");
+            for (int i = 0; i < frameCount; i++) {
+                Texture texture = new Texture(basePath + i + ".png");
                 frames.add(new TextureRegion(texture));
             }
-            animation = new Animation<>(0.5f/6f, frames, Animation.PlayMode.NORMAL);
+            animations.put(effectName, new Animation<>(speed / frameCount, frames, Animation.PlayMode.NORMAL));
+            frameCounts.put(effectName, frameCount);
         }
     }
 
-    public boolean isFinished() {
+    // Method to check if the effect is finished
+    public boolean isFinished(String effectName) {
+        Animation<TextureRegion> animation = animations.get(effectName);
         return animation.isAnimationFinished(stateTime);
     }
 
-
-    public void draw(SpriteBatch batch, float delta) {
+    // Draw the effect on screen
+    public void draw(SpriteBatch batch, float delta, String effectName) {
         stateTime += delta;
+        Animation<TextureRegion> animation = animations.get(effectName);
         TextureRegion frame = animation.getKeyFrame(stateTime, false);
         batch.draw(frame, x, y);
+    }
+
+    // Load a default effect with specified parameters (for convenience)
+    public static void loadDefaultEffect(String effectName, String basePath, int frameCount) {
+        loadAnimation(effectName, basePath, frameCount, defaultSpeed);
+    }
+
+    // Getter for animation frame count (optional)
+    public static int getFrameCount(String effectName) {
+        return frameCounts.get(effectName, 0);
     }
 }
