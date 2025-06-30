d4aa18ea01c6615c48cf7ac47ad86d050b77d30e
erfan <Erfansedaghat2321@gmail.com>
2025-06-30 14:12:48 +0330

    fix SFX

---
+++ core/src/main/java/com/Graphic/model/HelpersClass/SFX.java
@@ -0,0 +1,23 @@
+package com.Graphic.model.HelpersClass;
+
+public enum SFX {
+
+    test ("test","SFX/name.wav"),
+    ;
+
+    public final String name;
+    public final String path;
+
+    private SFX(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public String getPath() {
+        return path;
+    }
+}
