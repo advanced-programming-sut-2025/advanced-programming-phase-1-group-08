58a9341350ef4286c874117cf23ba4a6788cff52
erfan <Erfansedaghat2321@gmail.com>
2025-07-02 21:13:08 +0330

    add ForagingCrops path

--- core/src/main/java/com/Graphic/model/Plants/ForagingCrops.java
+++ core/src/main/java/com/Graphic/model/Plants/ForagingCrops.java
@@ -52,7 +52,4 @@ public class ForagingCrops extends Items {
     public String getIcon () {
         return this.type.getTexturePath();
     }
-    public Texture getIconTexture () {
-        return
-    }
 }
