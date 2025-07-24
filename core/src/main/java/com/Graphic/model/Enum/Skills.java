2363023df4daa63886b11f60f01a91394e0661eb
erfan <Erfansedaghat2321@gmail.com>
2025-07-24 19:53:19 +0330

    fix skill menu

--- core/src/main/java/com/Graphic/model/Enum/Skills.java
+++ core/src/main/java/com/Graphic/model/Enum/Skills.java
@@ -1,11 +1,13 @@
 package com.Graphic.model.Enum;
 
+import static com.Graphic.model.App.currentGame;
+
 public enum Skills {
 
-    Foraging ("this is Foraging ability"),
-    Farming  ("this is Farming ability"),
-    Fishing  ("this is Fishing ability"),
-    Mining   ("this is Mining ability");
+    Foraging ("this is Foraging ability \n Level : "),
+    Farming  ("this is Farming ability \n Level : "),
+    Fishing  ("this is Fishing ability \n Level : "),
+    Mining   ("this is Mining ability \n Level : ");
 
     private final String discription;
 
@@ -17,6 +19,14 @@ public enum Skills {
         return "Erfan/Skill/" + this.name() + " Skill Icon.png";
     }
     public String getDiscription() {
-        return discription;
+
+        if (this.equals(Foraging)) {
+            return discription + currentGame.currentPlayer.getForagingAbility();
+        } else if (this.equals(Farming)) {
+            return discription + currentGame.currentPlayer.getFarmingAbility();
+        } else if (this.equals(Fishing)) {
+            return discription + currentGame.currentPlayer.getFishingAbility();
+        } else
+            return discription + currentGame.currentPlayer.getMiningAbility();
     }
 }
